5933b2eb993ec13a9b52de7e867d0ec4af7373d5
HemanthKumar Mangala <hemanthkumar_mangala@epam.com>
2022-02-15 11:50:52 +0530

    Selenium

---
+++ restassured/src/main/java/TestAutomation/restassured/UserReceiver.java
@@ -0,0 +1,47 @@
+package TestAutomation.restassured;
+
+public class UserReceiver {
+	int id;
+	String name;
+	String email;
+	String profilepicture;
+	String location;
+	String createdat;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getProfilepicture() {
+		return profilepicture;
+	}
+	public void setProfilepicture(String profilepicture) {
+		this.profilepicture = profilepicture;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getCreatedat() {
+		return createdat;
+	}
+	public void setCreatedat(String createdat) {
+		this.createdat = createdat;
+	}
+	
+}
